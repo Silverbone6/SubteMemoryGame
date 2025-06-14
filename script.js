40a4f19379f62fd6c04113de8d91890e48fb3da2
Marcos Villena <mvillena@fi.uba.ar>
2025-06-14 13:49:59 -0300

    Archivos básicos agregados

---
+++ script.js
@@ -0,0 +1,8 @@
+var mapa = L.map('mapa').setView([-34.6037, -58.3816], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+    minZoom: 12,
+    maxZoom: 16,
+}).addTo(mapa);
